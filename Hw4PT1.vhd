6a0e6c2d573231b8e77677675b337dd37e8a2246
Dimitri Cromarty <dimitricromarty@Dimitris-MacBook-Air.local>
2025-04-01 18:35:47 -0400

    Syntax errors-upated

--- Hw4PT1.vhd
+++ Hw4PT1.vhd
@@ -9,25 +9,26 @@ entity prior_encoder_if_stmts is
     );
 end prior_encoder_if_stmts;
 architecture rtl of prior_encoder_if_stmts is
-    process(r) --whenever the r input changes this activates
     begin
-        code <= "00"; --base case
-        active <= '0';--base case
-        if(r(3) == '1')then --checks if the highest bit is equal to 1
-            code <= "11"; -- if yes set code to 11 and active to high 
-            active <= '1';
-        elsif(r(2) == '1')then--checks if the second highest bit is equal to 1
-            code <= "10"; --if yes set code to 10 and active to high 
-            active <= '1';
-        elsif(r(1) == '1')then --checks if third highest bit is equal to 1
-            code <= "01"; --If yes set code to 01 and active to 1
-            active <= '1';
-        elsif(r(0) == '1')then--checks if the last bit is equal to 1
-            code <= "00"; --If yes set code to 00 and active to 1
-            active <= '1';
-        else
-            code <= "00"; --set code to 00 and active to 0 if anyother case
-            active <= '0';
-        end if 
-    end process
+        process(r) --whenever the r input changes this activates
+            begin
+            code <= "00"; --base case
+            active <= '0';--base case
+            if(r(3) == '1')then --checks if the highest bit is equal to 1
+                code <= "11"; -- if yes set code to 11 and active to high 
+                active <= '1';
+            elsif(r(2) == '1')then--checks if the second highest bit is equal to 1
+                code <= "10"; --if yes set code to 10 and active to high 
+                active <= '1';
+            elsif(r(1) == '1')then --checks if third highest bit is equal to 1
+                code <= "01"; --If yes set code to 01 and active to 1
+                active <= '1';
+            elsif(r(0) == '1')then--checks if the last bit is equal to 1
+                code <= "00"; --If yes set code to 00 and active to 1
+                active <= '1';
+            else
+                code <= "00"; --set code to 00 and active to 0 if anyother case
+                active <= '0';
+            end if 
+        end process
 end rtl;
